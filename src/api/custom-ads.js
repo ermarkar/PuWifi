c0de72b4fa04563ecc8f06c57d18503e32bb478f
Sunil Kumar <sukumar@quark.com>
2017-12-21 13:15:25 +0530

    - Added more ads for demo - Added new api(\randomad) to serve random ads based on device and size

--- src/api/custom-ads.js
+++ src/api/custom-ads.js
@@ -2,9 +2,12 @@ var path = require("path");
 var _ = require("underscore");
 
 module.exports = function (router) {
-
+    var customAds = require("../data/custom_ads.json");
+    
+    /**
+     * To get the list of available and active custom ads
+     */
     router.get("/customads", (req, res) => {
-        var customAds = require("../data/custom_ads.json");
         var validMinAds = _.filter(customAds.minAds, (minAd) => {
             return new Date(minAd.endDate) > new Date();
         });
@@ -17,9 +20,38 @@ module.exports = function (router) {
         res.json(customAds);
     });
 
+    /**
+     * Server the ad based on device, size and id
+     */
     router.get("/getad/:size/:device/:id", (req, res) => {
-        res.sendFile(path.join(__dirname, "..", "..", process.env.STORAGE_IMAGE,
-            ((req.params.id + "_banner_" + req.params.size + "_" + req.params.device + ".jpg"))));
+        var filePath = path.join(__dirname, "..", "..", process.env.STORAGE_IMAGE,
+            ((req.params.id + "_banner_" + req.params.size + "_" + req.params.device + ".jpg")));
+        res.sendFile(filePath);
+    });
+
+    /**
+     * To serve the random ad by getting the random ad id from active ads, but requires device and size
+     */
+    router.get("/randomad/:size/:device", (req, res) => {
+        var validAds = {};
+        // get the valid ads based on size
+        if (req.params.size === "full") {
+            validAds = _.filter(customAds.fullAds, (fullAd) => {
+                return new Date(fullAd.endDate) > new Date();
+            });
+        } else {
+            validAds = _.filter(customAds.minAds, (minAd) => {
+                return new Date(minAd.endDate) > new Date();
+            });
+        }
+        // extract the array of valid ids
+        var adIds = _.pluck(validAds, "id");
+        var randomAdId = adIds[Math.floor(Math.random() * adIds.length)];
+
+        var filePath = path.join(__dirname, "..", "..", process.env.STORAGE_IMAGE,
+            ((randomAdId + "_banner_" + req.params.size + "_" + req.params.device + ".jpg")));
+
+        res.sendFile(filePath);
     });
 
     return router;
