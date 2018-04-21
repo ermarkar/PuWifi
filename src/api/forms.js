ff3b06a8f422ad433e2437158ad0e7a5da3322c0
Sunil Garg <er.markar@gmail.com>
2018-04-21 20:00:09 +0530

    Added apis to Get And Submit poll

--- src/api/forms.js
+++ src/api/forms.js
@@ -3,18 +3,40 @@ const db = require("../db/db-connect");
 
 module.exports = function (router) {
     /**
-     * Get the forms - surveys/registration/polls
+     * Get polls
      */
-    router.get("/forms", (req, res) => {
-        var formType = req.query.formType || 0;
+    router.get("/polls", (req, res) => {
         var deviceId = req.query.deviceId;
-        var query = "select * from get_forms(" + formType + ",'" + deviceId + "') as forms;";
+        var query = "select * from get_polls('" + deviceId + "') as polls;";
 
         db.query(query, (err, result) => {
             if (err) {
                 res.status(400).send(err);
             } else {
-                res.send({ data: result[0].forms });
+                res.send({ data: result[0].polls });
+            }
+        });
+    });
+
+    /**
+     * submit form handler
+     */
+    router.post("/pollsubmit", (req, res) => {
+        var deviceId = req.body.deviceId;
+        var pollId = req.body.pollId;
+        var pollResponse = JSON.stringify(req.body.pollResponse);
+        console.log(pollResponse)
+
+        if (!deviceId || !pollId) {
+            return res.send({ error: { message: "Required fields are missing." } });
+        }
+        var query = "select * from poll_submit('" + deviceId + "'," + pollId + ",'" + pollResponse + "') as result;";
+
+        db.query(query, (err, result) => {
+            if (err) {
+                res.status(400).send(err);
+            } else {
+                res.send({ data: result[0].result });
             }
         });
     });
