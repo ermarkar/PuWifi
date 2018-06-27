10e8c09c9bc74ff3a01528d89fb3924d166fd50e
er.markar@gmail.com <sukumar@quark.com>
2018-06-27 15:15:31 +0530

    Angular App routing will serve index.html page

--- src/api/services.js
+++ src/api/services.js
@@ -1,9 +1,16 @@
 var express = require('express');
 var router = express.Router();
+const path = require('path');
 
 module.exports = function (app) {
     app.use('/api/ads', require('./custom-ads')(router));
     app.use('/api/notification', require('./notifications')(router));
     app.use('/api/forms', require('./forms')(router));
     app.use('/api/mail', require('./mail')(router));
+
+    // Redirect to web app if call is not for an api
+    app.get('*', function (req, res) {
+        console.log(process.cwd())
+        res.sendFile( process.cwd() + '/public/index.html');
+    });
 }
